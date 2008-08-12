25ec123cc825623c0fb8bd18c590a4f7f35380c5
Rizqi Ahmad <raid_ox@yahoo.de>
2008-08-12 18:14:31 +0200

    Commit chain.js

---
+++ src/chain.js
@@ -0,0 +1,642 @@
+/**
+ * Chain.js
+ * 
+ * Data-Object Binding for jQuery.
+ *  
+ * @project 	Chain.js
+ * @author 		Rizqi Ahmad
+ * @license		LGPL
+ * 
+ */ 
+
+(function($){
+	
+$.Chain = 
+{
+	version: '0.1',
+	
+	tag: ['{', '}'],
+	
+	services: {},
+	
+	service: function(name, proto)
+	{
+		this.services[name] = proto;
+		
+		$.fn[name] = function(options)
+		{
+			if(!this.length) return this;
+			
+			var instance = this.data('chain-'+name);
+			var args = Array.prototype.slice.call(arguments, 1);
+			
+			if(!instance)
+			{
+				instance = $.extend({element: this}, $.Chain.services[name]);
+				this.data('chain-'+name, instance);
+				if(instance.init)
+					instance.init();
+			}
+			
+			var result;
+			
+			if(typeof options == 'string' && instance['$'+options])
+				result = instance['$'+options].apply(instance, args);
+			else if(instance['handler'])
+				result = instance['handler'].apply(instance, [options].concat(args));
+			else
+				result = this;
+			
+			if(options == 'destroy')
+				this.removeData('chain-'+name);
+			
+			return result;
+		};
+	},
+	
+	extend: function(name, proto)
+	{
+		if(this.services[name])
+			this.services[name] = $.extend(this.services[name], proto);
+	},
+	
+	jobject: function(obj)
+	{
+		return obj && obj.init == $.fn.init;
+	},
+	
+	jidentic: function(j1, j2)
+	{
+		if(!j1 || !j2 || j1.length != j2.length)
+			return false;
+		
+		a1 = j1.get();
+		a2 = j2.get();
+		
+		for(var i=0; i<a1.length; i++)
+			if(a1[i] != a2[i])
+				return false;
+		
+		return true;
+		
+	},
+	
+	parse: (function()
+	{
+		// Function Closure
+		var closure =
+		[
+			'var $text = [];\n'
+			+'$text.print = function(text)'
+			+'{this.push((typeof text == "number") ? text : ((typeof text != "undefined") ? text : ""));};\n'
+			+'with($data){\n',
+	
+			'}\nreturn $text.join("");'
+		];
+	
+		// Print text template
+		var textPrint = function(text)
+		{
+			return '$text.print("'
+				+text.split('\\').join('\\\\').split("'").join("\\'").split('"').join('\\"')
+				+'");';
+		};
+	
+		// Print script template
+		var scriptPrint = function(text)
+		{
+			return '$text.print('+text+');';
+		};
+	
+	
+		/*
+		 * Real function begins here.
+		 * We use closure for private variables and function.
+		 */
+		return function(text)
+		{
+			// Printing tag
+			var tag = $.Chain.tag;
+			
+			var opener, closer, closer2 = null, result = [];
+		
+			while(text){
+			
+				// Check where the opener and closer tag
+				// are located in the text.
+				opener = text.indexOf(tag[0]);
+				closer = opener + text.substring(opener).indexOf(tag[1]);
+			
+				// If opener tag exists, otherwise there are no tags anymore
+				if(opener != -1){
+					// Handle escape. Tag can be escaped with '\\'.
+					// If tag is escaped. it will be handled as a normal text
+					// Otherwise it will be handled as a script
+					if(text[opener-1] == '\\'){
+						closer2 = opener+tag[0].length + text.substring(opener+tag[0].length).indexOf(tag[0]);
+						if(closer2 != opener+tag[0].length-1 && text[closer2-1] == '\\')
+							closer2 = closer2-1;
+						else if(closer2 == opener+tag[0].length-1)
+							closer2 = text.length;
+					
+						result.push(textPrint(text.substring(0, opener-1)));
+						result.push(textPrint(text.substring(opener, closer2)));
+					}
+					else{
+						closer2 = null;
+						if(closer == opener-1)
+							closer = text.length;
+					
+						result.push(textPrint(text.substring(0, opener)));
+						result.push(scriptPrint(text.substring(opener+tag[0].length, closer)));
+					}
+						
+					text = text.substring((closer2 == null) ? closer+tag[1].length : closer2);
+				}
+				// If there are still text, it will be pushed to array
+				// So we won't stuck in an infinite loop
+				else if(text){
+					result.push(textPrint(text));
+					text = '';
+				}
+			}
+		
+			var fn;
+			// The parsed text will be transformed to a function.
+			// Exception handler is handling errors in parsing
+			// so, it won't make the whole program crash.
+			try{
+				fn = new Function('$data', closure[0]+result.join('\n')+closure[1]);
+			}catch(e){
+				throw 'error in parsing rules';
+				fn = function(){};
+			}
+		
+			fn.parsed = true;
+		
+			return fn;
+		};
+	})()
+};
+
+$.Chain.service('update', {
+	handler: function(obj)
+	{
+		if(typeof obj == 'function')
+			return this.bind(obj);
+		else
+			return this.trigger();
+	},
+	
+	bind: function(fn)
+	{
+		return this.element.bind('update', fn);
+	},
+	
+	trigger: function()
+	{
+		this.element.items('update');
+		this.element.item('update');
+		
+		this.element.triggerHandler('update', this.element.item());
+		this.element.triggerHandler('updated', this.element.item());
+		
+		return this.element;
+	}
+});
+
+$.Chain.service('chain', {
+	init: function()
+	{
+		this.anchor = this.element;
+		this.template = this.anchor.html();
+		this.builder = this.defaultBuilder;
+		this.plugins = {};
+		this.isActive = false;
+	},
+	
+	handler: function(obj)
+	{
+		this.element.items('backup');
+		this.element.item('backup');
+		
+		if(typeof obj == 'object')
+		{
+			this.build(obj);
+		}
+		else
+		{
+			if(typeof obj == 'function')
+				this.builder = obj;
+			else if(obj === true)
+				this.builder = this.defaultBuilder;
+			
+			this.anchor.empty();
+		}
+		
+		this.isActive = true;
+		this.element.update();
+		
+		return this.element;
+	},
+	
+	build: function(rules)
+	{
+		var builder = rules.builder;
+		delete rules.builder;
+		
+		this.setAnchor(rules.anchor);
+		delete rules.anchor;
+	
+		for(var i in rules)
+		{
+			if(typeof rules[i] == 'string')
+				rules[i] = $.Chain.parse(rules[i]);
+		}
+	
+		var fn = function(event, data)
+		{
+			var el, val;	
+			for(var i in rules)
+			{
+				el = $(i, this);
+			
+				if (typeof rules[i] == 'function')
+				{
+					val = rules[i].apply($(this), [data]);
+					if(typeof val == 'string')
+						el.text(val).val(val);
+				}
+			}
+		};
+	
+		this.builder = function()
+		{
+			if(builder)
+				builder.apply($(this));
+			$(this).update(fn);
+		};
+	},
+	
+	// Builder, not executable
+	defaultBuilder: function()
+	{
+		$(this).update(function(event, data){
+			var self = $(this);
+			for(var i in data)
+			{
+				self.find('.'+i).text(data[i]).val(data[i]).end();
+			}
+		});
+	},
+	
+	setAnchor: function(anchor)
+	{
+		this.anchor.html(this.template);
+		this.anchor = $(anchor || this.anchor);
+		this.template = this.anchor.html();
+		this.anchor.empty();
+	},
+	
+	$anchor: function(val)
+	{
+		if(val)
+		{
+			this.element.items('backup');
+			this.element.item('backup');
+			
+			this.setAnchor(val);
+			this.element.update();
+			
+			return this.element;
+		}
+		else
+		{
+			return this.anchor;
+		}
+	},
+	
+	$template: function()
+	{
+		return this.template;
+	},
+	
+	$builder: function(builder)
+	{
+		if(builder)
+			return this.handler(builder);
+		else
+			return this.builder;
+	},
+	
+	$active: function()
+	{
+		return this.isActive;
+	},
+	
+	$plugin: function(name, fn)
+	{
+		if(fn === null)
+			delete this.plugins[name];
+		else if(typeof fn == 'function')
+			this.plugins[name] = fn;
+		else if(name && !fn)
+			return this.plugins[name];
+		else
+			return this.plugins;
+		
+		this.element.items('backup');
+		this.element.item('backup');
+		
+		this.element.update();
+		
+		return this.element;
+	},
+	
+	$destroy: function()
+	{
+		this.element.items('backup');
+		this.element.item('backup');
+		
+		this.isActive = false;
+		
+		this.anchor.html(this.template);
+	}
+});
+
+$.Chain.service('items', {
+	init: function()
+	{
+		this.isActive = false;
+		this.buffer = [];
+	},
+	
+	handler: function(obj)
+	{
+		if(obj instanceof Array)
+			return this.$merge(obj);
+		else if($.Chain.jobject(obj))
+			return (!$.Chain.jidentic(obj, obj.item('root')) && $.Chain.jidentic(this.element, obj.item('root')))
+				? obj : $().eq(1);
+		else if(typeof obj == 'object')
+			return this.getByData(obj);
+		else if(typeof obj == 'number')
+			return this.getByNumber(obj);
+		else if(obj === true)
+			return this.element.chain('anchor').children('.chain-item');
+		else
+			return this.element.chain('anchor').children('.chain-item:visible');
+	},
+	
+	getByData: function(item)
+	{
+		return this.element.chain('anchor').children()
+			.filter(function(){return $(this).item() == item});
+	},
+	
+	getByNumber: function(number)
+	{
+		if(number == -1)
+			return this.element.chain('anchor').children(':last');
+		else
+			return this.element.chain('anchor').children(':eq('+number+')');
+	},
+	
+	update: function()
+	{
+		this.element.update();
+	},
+	
+	empty: function()
+	{
+		this.element.chain('anchor').empty();
+	},
+	
+	$update: function()
+	{
+		if(!this.element.chain('active') || !this.isActive)
+			return this.element;
+		
+		var self = this;
+		var builder = this.element.chain('builder');
+		var template = $(this.element.chain('template'));
+		
+		$.each(this.buffer, function(){
+			template
+				.clone()
+				.appendTo(self.element.chain('anchor'))
+				.addClass('chain-item')
+				.item('root', self.element)
+				.item(this)
+				.chain(builder);
+		});
+		
+		this.buffer = [];
+		
+		return this.element;
+	},
+	
+	$add: function()
+	{
+		this.isActive = true;
+		this.buffer = this.buffer.concat(Array.prototype.slice.call(arguments));
+		this.update();
+		
+		return this.element;
+	},
+	
+	$merge: function(items)
+	{
+		this.isActive = true;
+		this.buffer = this.buffer.concat(items);
+		this.update();
+		
+		return this.element;
+	},
+	
+	$replace: function(items)
+	{
+		this.isActive = true;
+		this.empty();
+		this.buffer = items;
+		this.update();
+		
+		return this.element;
+	},
+	
+	$remove: function()
+	{
+		for(var i=0; i<arguments.length; i++)
+			this.handler(arguments[i]).remove();
+		this.update();
+		
+		return this.element;
+	},
+	
+	$reorder: function(item1, item2)
+	{
+		this.handler(item1).before(this.handler(item2));
+		this.update();
+		
+		return this.element;
+	},
+	
+	$empty: function()
+	{
+		this.empty();
+		this.buffer = [];
+		this.update();
+		
+		return this.element;
+	},
+	
+	$data: function(x)
+	{
+		return this.handler(x).map(function(){return $(this).item();}).get();
+	},
+	
+	$active: function()
+	{
+		return this.isActive;
+	},
+	
+	$backup: function()
+	{
+		if(!this.element.chain('active') || !this.isActive)
+			return;
+		
+		var buffer = [];
+		this.element.chain('anchor').children().each(function(){
+			var item = $(this).item();
+			if(item)
+				buffer.push(item);
+		});
+		this.buffer = buffer.concat(this.buffer);
+		
+		this.empty();
+		
+		return this.element;
+	},
+	
+	$destroy: function()
+	{
+		this.empty();
+		return this.element;
+	}
+});
+
+$.Chain.service('item', {
+	init: function()
+	{
+		this.isActive = false;
+		this.isBuilt = false;
+		this.root = this.element;
+		this.data = false;
+		this.datafn = this.dataHandler;
+	},
+	
+	handler: function(obj)
+	{
+		if(typeof obj == 'object')
+		{
+			this.data = this.datafn.call(this.element, this.data || obj, obj);
+			this.isActive = true;
+			
+			this.update();
+			
+			return this.element;
+		}
+		else if(typeof obj == 'function')
+		{
+			this.datafn = obj;
+			this.update();
+		}
+		
+		if(this.isActive)
+			return this.datafn.call(this.element, this.data);
+		else
+			return false;
+	},
+	
+	dataHandler: function(a, b)
+	{
+		if(b)
+			return $.extend(a, b);
+		else
+			return a;
+	},
+	
+	update: function()
+	{
+		this.element.update();
+	},
+	
+	build: function()
+	{
+		this.element.chain('anchor').html(this.element.chain('template'));
+		
+		if(!$.Chain.jidentic(this.root, this.element))
+		{
+			var plugins = this.root.chain('plugin');
+			for(var i in plugins)
+				plugins[i].apply(this.element);
+			
+		}
+		
+		this.element.chain('builder').apply(this.element.chain('anchor'));
+		this.isBuilt = true;
+		
+		var self = this;
+	},
+	
+	$update: function()
+	{
+		if(this.element.chain('active') && this.isActive && !this.isBuilt && this.data)
+			this.build();
+		
+		return this.element;
+	},
+	
+	$replace: function()
+	{
+		this.data = obj;
+		this.isActive = true;
+		return this.element;
+	},
+	
+	$remove: function()
+	{
+		this.element.remove();
+		if(this.root)
+			this.root.update();
+		this.root = null;
+	},
+	
+	$active: function()
+	{
+		return this.isActive;
+	},
+	
+	$root: function(val)
+	{
+		if(arguments.length)
+		{
+			this.root = val;
+			this.update();
+			return this.element;
+		}
+		else
+		{
+			return this.root;
+		}
+	},
+	
+	$backup: function()
+	{
+		this.isBuilt = false;
+	}
+	
+	
+});
+	
+})(jQuery);
